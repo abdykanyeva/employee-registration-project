b6a62966f0b83ce6a106df42fbb06f3a886606b6
Adelina <adelina.abdykanyeva@gmail.com>
2022-09-28 23:29:58 -0500

    Employee lab complete

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -3,15 +3,27 @@ package com.cydeo.controller;
 
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/register")
     public String createEmployee(Model model){
@@ -21,6 +33,28 @@ public class EmployeeController {
 
 
         return "employee/employee-create";
+    }
+
+
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee")@Valid Employee employee, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "employee/employee-create";
+        }
+
+
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/employee/list"; // with redirect we are using endpoints
+
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        model.addAttribute("employeeList", employeeService.readAllEmployees());
+        return "employee/employee-list";  // without redirect we are using html file paths
+
     }
 
 
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,7 +1,12 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 
@@ -12,8 +17,20 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+
+//    @NotNull    ----> Field shouldn't be null
+//    @NotEmpty   ----> Field shouldn't be ""
+//    @NotBlank   ----> Field should not be blank
+
+//    @NotNull  -> @NotNul
+//    @NotEmpty -> @NotNull +@NotEmpty
+//    @NotBlank --> @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;
